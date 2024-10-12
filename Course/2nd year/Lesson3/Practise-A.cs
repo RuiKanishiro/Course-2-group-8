4caccc89fb0f94debb0affa0df2cda2bd4d7c5b4
RuiKanishiro <150694738+RuiKanishiro@users.noreply.github.com>
2024-10-12 19:55:23 +0500

    Практика А: урок 3

---
+++ Course/2nd year/Lesson3/Practise-A.cs
@@ -0,0 +1,17 @@
+﻿using System;
+ 
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[5];
+            string[] mas = new string[3];
+            mas[0] = "Tom";
+            mas[1] = "Tam";
+            mas[2] = "Tim";
+            Console.WriteLine(mas[2]);
+        }
+    }
+}
